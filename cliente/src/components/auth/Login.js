c45813e223d992c5991967ab6f5015f763aa12ea
patorma <pocontreras@ing.ucsc.cl>
2021-02-14 22:08:36 -0300

    se termino interfaz login

--- cliente/src/components/auth/Login.js
+++ cliente/src/components/auth/Login.js
@@ -1,14 +1,48 @@
+import {useState} from 'react';
+import { Link } from 'react-router-dom';
+ 
 const Login = () => {
 
-    const onChange = () =>{
+    // State para iniciar sesión
+    // el parametro es el valor inicial 
+    const [usuario, guardarUsuario] = useState({
+        email:'',
+        password: ''
+    })
+   // extraer usuario
+   const {email, password} = usuario; 
+    const onChange = e =>{
+        guardarUsuario({
+            //se toma una copia de usuario
+            // para que lo que este escrito en el otro campo o
+            // en otra pieza de state no se reescriba
+            ...usuario,
+            //y se reescribe la actual con:
+            [e.target.name] : e.target.value
 
+        })
     }
+
+   // cuando el usuario quiere iniciar sesión
+    const onSubmit = e =>{
+       e.preventDefault();
+
+       //  Validar que no haya campos vacios 
+
+
+       //  Pasarlo al action 
+    }
+
+
+
     return ( 
         <div className="form-usuario">
             <div className="contenedor-form sombra-dark">
                 <h1>Iniciar Sesión</h1>
 
-                <form>
+                <form 
+                   onSubmit={onSubmit}
+                >
                     <div className="campo-form">
                         <label htmlFor="email">Email</label>
                         <input 
@@ -16,6 +50,7 @@ const Login = () => {
                            id="email"
                            name="email"
                            placeholder="Tu Email"
+                           value={email}
                            onChange={onChange}
                         />
                     </div>
@@ -27,6 +62,7 @@ const Login = () => {
                            id="password"
                            name="password"
                            placeholder="Tu Password"
+                           value={password}
                            onChange={onChange}
                         />
                     </div>
@@ -37,6 +73,9 @@ const Login = () => {
                     </div>
                 </form>
 
+                  <Link to={'/nueva-cuenta'} className="enlace-cuenta">
+                      Obtener cuenta
+                  </Link>
             </div>
         </div>
      );
